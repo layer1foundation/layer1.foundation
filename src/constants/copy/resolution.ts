529ab0832f2952d979a43a03a8f9185b95def98e
fantomă <mrfakie789@gmail.com>
2024-03-07 00:19:17 -0500

    completed resolution page

---
+++ src/constants/copy/resolution.ts
@@ -0,0 +1,31 @@
+import { IMAGES } from "../links";
+
+const copy = {
+    title: "Commitment to Priniples",
+    author: {
+        img: "/android-chrome-144x144.png",
+        name: "L1F",
+        description: "Guiding pillars for brc20 protocol development",
+    },
+    date: "March 7, 2024",
+    readtime: "5 min",
+    image: IMAGES.governance.desktop,
+
+    list: `We commit to the pursuit of simplicity in our technical architecture whenever possible, except where doing so might pose potential hazards to BRC20 assets. We strive for an uncomplicated protocol design to enhance accessibility and ease of use—and to prevent technical debts or complications as the protocol evolves. 
+    We commit to a safety-first approach when making changes to the protocol or its indexers. We prioritize the security and reliability of the protocol by ensuring rigorous testing and validation—and will collaborate on timely identification and resolution of potential vulnerabilities. 
+    We commit to fostering mutual discussion and considering our community when pursuing changes in development. Major changes to the index will be thoroughly discussed amongst partners within at least one difficulty adjustment (~2 weeks), ensuring that consensus-building is a key element in shaping the protocol.
+    We commit to making protocol-level changes open-source—enabling transparency and scrutiny from the wider community. This transparency not only builds trust among stakeholders but ensures that decision-making is inclusive of a diverse range of user interests. 
+    We commit to the broader value of decentralization—emphasizing that any changes to the protocol should prioritize the protocol's best interests. Updates should not seek to benefit individual entities or organizations but rather serve the collective interests of BRC20 users.`,
+
+    description: `The Layer 1 Foundation and its partners recognize the critical importance of fostering a collaborative approach in the ongoing development and maintenance of the BRC20 protocol. To achieve this, the Layer 1 Foundation and its partners put forth the following resolution as an outline of key principles we aim to pursue as collective maintainers of this technology.`,
+
+    content: `By prioritizing these pillars, we strive to create a resilient and user-friendly ecosystem—fostering trust within the broader Bitcoin community. Users must always come first, and through this resolution, we aim to ensure that the BRC20 protocol remains reliable and inclusive.
+
+Additionally, partners will refer to the following Standard Operating Procedure for specific suggestions on how we may embody these principles as we co-develop this technology. 
+
+Together, we affirm our steadfast dedication to safeguarding the well-being of BRC20 users—nurturing innovation while respecting user sovereignty, safety, and usability.
+
+Our current signatory parties include: Layer 1 Foundation, Domo, Unisat, Best In Slot, Oyl Dynamics, ALEX, UTXO Management, Allium & Hiro`,
+};
+
+export default copy;
---
+++ src/constants/copy/sop.ts
@@ -0,0 +1,31 @@
+import { IMAGES } from "../links";
+
+const copy = {
+    title: "A Letter from Domo",
+    author: {
+        img: "/partners/domo-color.svg",
+        name: "DOMO",
+        description:
+            "Creator of the BRC20 Protocol and President of the Layer 1 Foundation",
+    },
+    date: "March 6, 2024",
+    readtime: "5 min",
+    image: IMAGES.letter.desktop,
+    description: `Dear Community,
+
+    It has been an incredible year since launching my 'worthless' experiment and its success is entirely due to the community's dedication to embracing Bitcoin's fair, open, and experimental ethos. I am immensely grateful for this support.
+  
+  As we move forward, the potential of the Bitcoin metaprotocol space is more exciting than ever. L1F is positioned as a leading hub, catering to both experienced protocol creators and new users. Our collective goal is to ensure that L1F meets the needs of users and developers regardless of their stage in the metaprotocol journey.`,
+    content: `We hope to support metaprotocol development through sharing tools, standards, and educational resources—as well as by advocating for fair and decentralized governance of these technologies wherever possible and appropriate.
+
+  To ensure effective progress in this direction, governance of BRC20 will transition to a maintainership model, spearheaded by Best in Slot and Unisat, and guided by a diverse committee of stakeholders—including myself, Hiro, Oyl Dynamics, ALEX Labs, and UTXO Management.
+  
+  To learn more about how we are committing to a more decentralized and ethos-driven governance model for BRC20, feel free to read our Governance Resolution and Standard Operating Procedures for future BRC20 protocol development.
+  
+  With these guidelines in place, we hope to unlock the full potential of an account model on Bitcoin and spur innovation on Bitcoin and beyond. I believe we have only scratched the surface of what is possible.
+  
+  Thank you!
+  domo`,
+};
+
+export default copy;
