2ebf15c82378ad51a261db7df068dca99a30dc6a
fantomă <mrfakie789@gmail.com>
2024-03-07 08:58:27 -0500

    try

--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -154,7 +154,7 @@ export default function Tools() {
             <Segment
                 scroll={false}
                 className="border-b mb-20 md:mb-0"
-                id={getIdFromLink(INTERNAL_LINKS.tools.xverse)}
+                id={"xverse"}
             >
                 <Chapter chapter={copy.tools.chapterXverse} />
             </Segment>
